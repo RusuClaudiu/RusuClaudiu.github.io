23418e56bcd91f8b0a1991f134959edc74548818
Vladimir Urucu <vladimir.urucu@gmail.com>
2024-06-02 13:09:40 +0300

    add a lot of changes

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ function userScroll(){
     const navbar = document.querySelector('.navbar');
 
     window.addEventListener('scroll', () =>{
-        if(window.scrollY > 50){
+        if(window.scrollY >10){
             navbar.classList.add('bg-dark'); 
             navbar.classList.add('navbar-sticky');    
         }else{
-            navbar.classList.remove('bg-dark');
-            navbar.classList.remove('navbar-sticky');  
+            // navbar.classList.remove('bg-dark');
+            // navbar.classList.remove('navbar-sticky');  
         }
     })
 }
